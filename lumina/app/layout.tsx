1688c5ad340587c4966a7c92b36a20db53708689
mroxso <24775431+mroxso@users.noreply.github.com>
2024-08-04 15:50:19 +0200

    fix umami script loading

--- lumina/app/layout.tsx
+++ lumina/app/layout.tsx
@@ -7,6 +7,7 @@ import { TopNavigation } from "@/components/headerComponents/TopNavigation";
 import BottomBar from "@/components/BottomBar";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster"
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "LUMINA",
@@ -26,7 +27,7 @@ export default function RootLayout({
       <Head>
         <link rel="icon" href="/icon?<generated>" type="image/png" sizes="32x32" />
         <link rel="manifest" href="/manifest.json" />
-        <script defer src="https://umami.softwerk.cloud/script.js" data-website-id="d0495d49-1f04-4501-8711-3bdcb4f7f7bd"></script>
+        {/* <script defer src="https://umami.softwerk.cloud/script.js" data-website-id="d0495d49-1f04-4501-8711-3bdcb4f7f7bd"></script> */}
       </Head>
       <body className={inter.className}>
         <ThemeProvider
@@ -37,6 +38,12 @@ export default function RootLayout({
         >
           <TopNavigation />
           <Toaster />
+          <Script
+            src="https://umami.softwerk.cloud/script.js"
+            strategy="afterInteractive"
+            data-website-id="d0495d49-1f04-4501-8711-3bdcb4f7f7bd"
+            defer
+          />
           <div className="main-content pb-14">
             {children}
           </div>
