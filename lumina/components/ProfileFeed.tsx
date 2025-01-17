bd0542a8a09d907e74f7010ca72966302e02d37b
mr0x50 <24775431+mroxso@users.noreply.github.com>
2025-01-18 00:26:20 +0100

    update profile feed to use kind 20 and kind 20 cards (NIP-68 Picture-first feeds)

--- lumina/components/ProfileFeed.tsx
+++ lumina/components/ProfileFeed.tsx
@@ -2,6 +2,8 @@ import { useRef } from "react";
 import { useNostrEvents, dateToUnix } from "nostr-react";
 import NoteCard from '@/components/NoteCard';
 import { Skeleton } from "@/components/ui/skeleton";
+import KIND20Card from "./KIND20Card";
+import { getImageUrl } from "@/utils/utils";
 
 interface ProfileFeedProps {
   pubkey: string;
@@ -16,19 +18,15 @@ const ProfileFeed: React.FC<ProfileFeedProps> = ({ pubkey }) => {
       authors: [pubkey],
       // since: 0,
       // limit: 10,
-      kinds: [1],
+      kinds: [20],
     },
   });
-
-  let filteredEvents = events.filter((event) => event.content.match(/https?:\/\/.*\.(?:png|jpg|gif|mp4|webm|mov|jpeg)/g)?.[0]);
-  // filter out all replies (tag[0] == e)
-  filteredEvents = filteredEvents.filter((event) => !event.tags.some((tag) => { return tag[0] == 'e' }));
 
   return (
     <>
       {/* <h2>Profile Feed</h2> */}
 
-      {filteredEvents.length === 0 ? (
+      {events.length === 0 ? (
         <div className="flex flex-col space-y-3">
           <Skeleton className="h-[125px] rounded-xl" />
           <div className="space-y-2">
@@ -36,11 +34,11 @@ const ProfileFeed: React.FC<ProfileFeedProps> = ({ pubkey }) => {
             <Skeleton className="h-4 w-[200px]" />
           </div>
         </div>
-      ) : (filteredEvents.map((event) => (
+      ) : (events.map((event) => (
         // <p key={event.id}>{event.pubkey} posted: {event.content}</p>
         // <ProfileNoteCard key={event.id} pubkey={event.pubkey} text={event.content} event={event} tags={event.tags} />
         <div key={event.id} className="py-6">
-          <NoteCard key={event.id} pubkey={event.pubkey} text={event.content} event={event} tags={event.tags} eventId={event.id} showViewNoteCardButton={true}/>
+          <KIND20Card key={event.id} pubkey={event.pubkey} text={event.content} image={getImageUrl(event.tags)} event={event} tags={event.tags} eventId={event.id} showViewNoteCardButton={true}/>
         </div>
       )))}
     </>
