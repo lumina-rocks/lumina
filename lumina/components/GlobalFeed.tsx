1ec2c83c24c57cb46fa04e8a2c63a4438a5bb417
mr0x50 <24775431+mroxso@users.noreply.github.com>
2025-01-17 23:05:21 +0100

    refactor: move getImageUrl function to utils for better modularity

--- lumina/components/GlobalFeed.tsx
+++ lumina/components/GlobalFeed.tsx
@@ -1,16 +1,6 @@
 import { useNostrEvents } from "nostr-react";
 import KIND20Card from "./KIND20Card";
-
-export function getImageUrl(tags: string[][]): string {
-  const imetaTag = tags.find(tag => tag[0] === 'imeta');
-  if (imetaTag) {
-    const urlItem = imetaTag.find(item => item.startsWith('url '));
-    if (urlItem) {
-      return urlItem.split(' ')[1];
-    }
-  }
-  return '';
-}
+import { getImageUrl } from "@/utils/utils";
 
 const GlobalFeed: React.FC = () => {
   const { events } = useNostrEvents({
