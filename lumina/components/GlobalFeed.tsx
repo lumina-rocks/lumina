447d1ed519e5531083d7b1f4fcfc32145061b922
mr0x50 <24775431+mroxso@users.noreply.github.com>
2025-01-18 02:58:34 +0100

    global feed show items in grid

--- lumina/components/GlobalFeed.tsx
+++ lumina/components/GlobalFeed.tsx
@@ -1,6 +1,7 @@
 import { useNostrEvents } from "nostr-react";
 import KIND20Card from "./KIND20Card";
 import { getImageUrl } from "@/utils/utils";
+import QuickViewKind20NoteCard from "./QuickViewKind20NoteCard";
 
 const GlobalFeed: React.FC = () => {
   const { events } = useNostrEvents({
@@ -13,23 +14,35 @@ const GlobalFeed: React.FC = () => {
   return (
     <>
       <h2>Global Feed</h2>
-      {events.map((event) => {
-        const imageUrl = getImageUrl(event.tags);
-        return (
-          <div key={event.id} className="py-6">
-            <KIND20Card 
-              key={event.id} 
-              pubkey={event.pubkey} 
-              text={event.content} 
-              image={imageUrl} 
-              eventId={event.id} 
-              tags={event.tags} 
-              event={event} 
-              showViewNoteCardButton={true} 
-            />
-          </div>
-        );
-      })}
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 space-x-2">
+        {events.map((event) => {
+          const imageUrl = getImageUrl(event.tags);
+          return (
+            <div key={event.id} className="py-6">
+              <KIND20Card
+                key={event.id}
+                pubkey={event.pubkey}
+                text={event.content}
+                image={imageUrl}
+                eventId={event.id}
+                tags={event.tags}
+                event={event}
+                showViewNoteCardButton={true}
+              />
+              {/* <QuickViewKind20NoteCard
+                key={event.id}
+                pubkey={event.pubkey}
+                text={event.content}
+                image={imageUrl}
+                eventId={event.id}
+                tags={event.tags}
+                event={event}
+                linkToNote={true}
+                /> */}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
