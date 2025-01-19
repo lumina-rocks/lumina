75febc128562ffbe937a3a53baf64e4c46ad54cb
mr0x50 <24775431+mroxso@users.noreply.github.com>
2025-01-19 17:59:24 +0100

    use nextjs Link component instead of javascript for the ViewNoteButton component

--- lumina/components/ViewNoteButton.tsx
+++ lumina/components/ViewNoteButton.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { SizeIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 import { nip19 } from "nostr-tools";
 
 export default function ViewNoteButton({ event }: { event: any }) {
     const encodedNoteId = nip19.noteEncode(event.id);
     return (
-        <Button variant="secondary" onClick={() => window.location.href = '/note/'+encodedNoteId}><SizeIcon /></Button>
+        <Link href={'/note/' + encodedNoteId} passHref>
+            <Button variant="secondary"><SizeIcon /></Button>
+        </Link>
     );
 }
