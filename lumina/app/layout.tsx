0f9664a18b72797fca568c3c3500ea3c4f457b5b
mr0x50 <24775431+mroxso@users.noreply.github.com>
2025-02-02 11:48:34 +0100

    WIP

--- lumina/app/layout.tsx
+++ lumina/app/layout.tsx
@@ -10,6 +10,7 @@ import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster"
 import Script from "next/script";
 import Umami from "@/components/Umami";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  useEffect(() => {
+    // handle Amber Sign Response
+    const urlParams = new URLSearchParams(window.location.search);
+    const amberSignResponse = urlParams.get('amberSignResponse');
+    if (amberSignResponse !== null) {
+      alert(amberSignResponse);
+    }
+  }, []);
 
   const relayUrls = [
     "wss://relay.nostr.band",
