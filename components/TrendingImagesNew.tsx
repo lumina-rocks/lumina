4e0abef83a75004c1413e3bbdd40086b88c58d86
highperfocused <24775431+mroxso@users.noreply.github.com>
2025-02-25 18:11:01 +0100

    add loading spinner and message for empty state

--- components/TrendingImagesNew.tsx
+++ components/TrendingImagesNew.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import TrendingImage from '@/components/TrendingImageNew';
+import { Spinner } from '@/components/spinner';
 
 export function TrendingImagesNew() {
     const [events, setEvents] = useState<any[]>([]);
@@ -18,9 +19,16 @@ export function TrendingImagesNew() {
         <div className="flex flex-col items-center py-6 px-6">
             <h1 className="text-3xl font-bold">Currently Trending</h1>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-6">
-                {events && events.length > 0 && events.map((event, index) => (
-                    <TrendingImage key={event.id} event={event} />
-                ))}
+                {events && events.length > 0 ? (
+                    events.map((event, index) => (
+                        <TrendingImage key={event.id} event={event} />
+                    ))
+                ) : (
+                    <div className="col-span-full text-center py-8 text-lg flex flex-col items-center gap-4">
+                        <Spinner />
+                        Curating Trending Images for you.. 💜
+                    </div>
+                )}
             </div>
         </div>
     );
