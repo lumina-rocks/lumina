986e1519480395bfe7d47dee44542f932a091a8c
mroxso <24775431+mroxso@users.noreply.github.com>
2025-04-21 16:28:55 +0200

    Design: Small Design Updates for NoteCard (#91)
    * fix: remove unnecessary padding from NotePage and KIND20Card components
    
    * fix: remove unnecessary class from KIND20Card component
    
    ---------
    
    Co-authored-by: highperfocused <highperfocused@pm.me>

--- app/note/[id]/page.tsx
+++ app/note/[id]/page.tsx
@@ -26,7 +26,7 @@ export default function NotePage() {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <div className="py-6 px-6">
+        <div className="px-6">
           <div className="pb-6">
             <NotePageComponent id={id.toString()} />
           </div>
