120f166a45b0e2bb7fe1b70f98ba264f7ec30717
highperfocused <highperfocused@pm.me>
2025-05-06 18:17:06 +0200

    fix: add header cors for well-known public folder

--- next.config.mjs
+++ next.config.mjs
@@ -5,17 +5,31 @@ const config = {
 };
 
 const nextConfig = {
-    output: 'standalone',
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: '**',
-            port: '',
-            pathname: '/**',
-          },
-        ],
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '/**',
       },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // matching all well-known routes
+        source: "/.well-known/:path*",
+        headers: [
+          // { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+          // { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 };
 
 export default withPWA(config)(nextConfig);
