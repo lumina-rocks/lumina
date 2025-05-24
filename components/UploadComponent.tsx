bca7f09129b1800e7ac208f503fee7b855b9defe
highperfocused <highperfocused@pm.me>
2025-05-24 23:08:21 +0200

    fix: Remove deprecated server option from UploadComponent

--- components/UploadComponent.tsx
+++ components/UploadComponent.tsx
@@ -441,7 +441,6 @@ const UploadComponent: React.FC = () => {
                   <SelectContent>
                     <SelectItem value="blossom.band">blossom.band</SelectItem>
                     <SelectItem value="blossom.primal.net">blossom.primal.net</SelectItem>
-                    <SelectItem value="media.lumina.rocks">media.lumina.rocks</SelectItem>
                   </SelectContent>
                 </Select>
               </div>
