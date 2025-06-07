7092821880f4f95cda0a0cb3a72000cd7562c01a
mroxso <24775431+mroxso@users.noreply.github.com>
2025-06-07 19:18:24 +0200

    add better icon (#132)
    Co-authored-by: highperfocused <highperfocused@pm.me>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Metadata } from "next";
 import "./globals.css";
 import { NostrProvider } from "nostr-react";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -8,7 +7,6 @@ import { TopNavigation } from "@/components/headerComponents/TopNavigation";
 import BottomBar from "@/components/BottomBar";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster"
-import Script from "next/script";
 import Umami from "@/components/Umami";
 import { useEffect, useState } from "react";
 
@@ -48,7 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/icon?<generated>" type="image/png" sizes="32x32" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="manifest" href="/manifest.json" />
         <title>LUMINA</title>
         <meta name="description" content="An effortless, enjoyable, and innovative way to capture, enhance, and share moments with everyone, decentralized and boundless." />
