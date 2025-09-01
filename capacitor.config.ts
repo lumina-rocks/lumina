b258bac4837155baa810c5f15d32bde82ba0b91b
highperfocused <highperfocused@pm.me>
2025-09-01 22:01:57 +0200

    chore: update .dockerignore and .gitignore for Capacitor, add build scripts to package.json, and create initial index.html

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'rocks.lumina.app',
   appName: 'LUMINA',
-  webDir: 'public'
+  webDir: 'public',
+  server: {
+    url: 'https://lumina.rocks',
+    cleartext: true
+  }
 };
 
 export default config;
