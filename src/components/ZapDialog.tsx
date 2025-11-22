cb15904081e0a6fd86e234f69a28b08e9b23985e
highperfocused <highperfocused@pm.me>
2025-11-22 21:18:21 +0100

    Update comment in ZapDialog to reflect new branding message

--- src/components/ZapDialog.tsx
+++ src/components/ZapDialog.tsx
@@ -251,7 +251,7 @@ export function ZapDialog({ target, children, className }: ZapDialogProps) {
 
   useEffect(() => {
     if (target) {
-      setComment('Zapped with MKStack!');
+      setComment('Zapped with LUMINA.rocks');
     }
   }, [target]);
 
